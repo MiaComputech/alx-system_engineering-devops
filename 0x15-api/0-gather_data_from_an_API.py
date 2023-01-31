525ed8a8acddc35cb6bd06ecff32ac9e78403c01
MiaComputech <MiaComputechplc@gmail.com>
2023-01-31 22:08:11 +0300

    task all' '

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-""" Api """
-
+"""getting data from an api
+"""
 
 import requests
-import sys
+from sys import argv
 
-
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/users/"
-    user_id = sys.argv[1]
-    user = requests.get(url + user_id).json()
-    todo = requests.get(url + user_id + "/todos").json()
-    name = user.get("name")
-    username = user.get("username")
-    tasks = []
+if __name__ == '__main__':
+    endpoint = "https://jsonplaceholder.typicode.com"
+    userId = argv[1]
+    user = requests.get(endpoint + "users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get(endpoint + "todos?userId={}".
+                        format(userId), verify=False).json()
+    completed_tasks = []
     for task in todo:
-        if user.get('id') == int(user_id):
-            tasks.append(task.get("title"))
-            break
-        completed = task.get("completed")
-        with open("{}.csv".format(user_id), "a") as f:
-            f.write('"{}","{}","{}","{}"'.format(user_id, username, completed,
-                                                 name))
-            f.write("")
-    print("Employee {} is done with tasks({}/{}):".format(name, len(tasks),
-                                                          len(todo)))
-    for task in tasks:
-        print("\t {}".format(task))
+        if task.get('completed') is True:
+            completed_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(completed_tasks), len(todo)))
+    print("\n".join("\t {}".format(task) for task in completed_tasks))
